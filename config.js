62e145c6a04c41ee29b9bfb0e9b91a8d1b16dba8
Espe <ego.egarco01@gmail.com>
2020-05-27 14:49:12 +0200

    add routes

---
+++ config.js
@@ -0,0 +1,20 @@
+
+  const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY || envs.FIREBASE_API_KEY,
+  authDomain: process.env.AUTH_DOMAIN || envs.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL || envs.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID || envs.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET || envs.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || envs.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID || envs.FIREBASE_APP_ID
+  };
+
+  const mongoConfig = process.env.MONGO_DB_URL || envs.MONGO_DB_URL;
+
+  
+const config = {
+  firebaseConfig,
+  mongoConfig
+}
+
+module.exports = config;
---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const PORT = process.env.PORT || 8080
+const config = require('./config')
+
+app.use(express.json())
+app.use(cors())
+
+const articlesRoute= require('./routes/articles')
+const usersRoute= require('./routes/users')
+const authRoute= require('./routes/auth')
+
+app.use(articlesRoute)
+app.use(usersRoute)
+app.use(authRoute)
+
+firebase.initializeApp(config.firebaseConfig)
+
+async function connectDatabase() {
+  let db = mongoose.connection;
+  
+  try {
+    await mongoose.connect(config.mongoConfig, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  } catch (err) {
+    console.log("Imposible conectar a la base de datos");
+    console.log(err);
+  }
+}
+
+async function init() {
+  await connectDatabase();
+  app.listen(PORT, () => console.log(`Conectado al puerto ${PORT}`));
+}
+
+init();
